83a4380b35423a22c2cd13c4de746c7b0feb7691
pranavv481 <pranavv481@gmail.com>
2020-05-24 19:34:21 +0530

    context api

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/UserContext';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <UserProvider value="Vishwas">
+          <ComponentC />
+        </UserProvider>
+      </div>
+    );
+  }
+}
+
+export default App;
